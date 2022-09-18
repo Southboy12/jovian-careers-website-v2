6032678066769239783dee1c50611578abdd6e97
chimerenka chikwendu <112117995+Southboy12@users.noreply.github.com>
2022-09-18 12:20:08 +0000

    added dynamic page to each job

--- database.py
+++ database.py
@@ -14,9 +14,17 @@ engine = create_engine(
 
 def load_jobs_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from   jobs"))
+    result = conn.execute(text("select * from jobs"))
     jobs = []
     for row in result.all():
       jobs.append(dict(row))
     return jobs
-#print(result_dicts)
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), val=id)
+    row = result.all()
+    if len(row) == 0:
+      return None
+    else:
+      return dict(row[0])
